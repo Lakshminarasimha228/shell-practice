79c61562d8fa5b385ac666df6b611d26c6a514fa
lakshmi <lakshminarasimha228@gmail.com>
2025-06-26 00:03:48 +0530

    hello world started

--- 4-Variable.sh
+++ 4-Variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-PERSON1=Modi
-PERSON2=Mask
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1  :: Hey $PERSON2, How are you?"
 echo "$PERSON2  :: Hello $PERSON1, I am fine. How are you doing"
