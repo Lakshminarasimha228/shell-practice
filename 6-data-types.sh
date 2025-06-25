87dbd4f92fafc7e0ed9b78b7bdb5ff870fba121a
lakshmi <lakshminarasimha228@gmail.com>
2025-06-26 00:27:30 +0530

    hello world started

--- 6-data-types.sh
+++ 6-data-types.sh
@@ -4,7 +4,7 @@ NUMBER1=100
 NUMBER2=200
 
 TIMESTAMP=$(date)
-$Narasimha=$(weather)
-echo "Script executeed at : $TIMESTAMP"
-echo "lakshmi at : $Narasimha"
+$Narasimha=$(date)
+echo "Script executed at : $TIMESTAMP"
+echo "Lakshmi at : $Narasimha"
 #SUM=$NUMBER1+$NUMBER2
