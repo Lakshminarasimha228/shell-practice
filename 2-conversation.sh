5d3e961715d87178c29496bfc9ad1ed7dcf4f392
lakshmi <lakshminarasimha228@gmail.com>
2025-06-25 23:34:21 +0530

    hello world started

--- 2-conversation.sh
+++ 2-conversation.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-person1=Trump
-person2=mod
 
-echo "Trump :: Hey Mask, How are you?"
-echo "Mask  :: Hello Trump, I am fine. How are you doing"
-echo "Trump :: I am fine too. What's up?"
+echo "Trump  :: Hey Mask, How are you?"
+echo "Mask  :: Hello  Trump, I am fine. How are you doing"
+echo "Trump  :: I am fine too. What's up?"
 echo "Mask  :: Nothing. Just going to Mars now. Will you come?"
-echo "Trump :: Sorry. Carry on!. I will come once you come back."
+echo "Trump  :: Sorry. Carry on!. I will come once you come back."
