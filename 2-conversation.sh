eac2795298353c089dfab11e7df89267331f414c
lakshmi <lakshminarasimha228@gmail.com>
2025-06-25 23:15:17 +0530

    hello world started

---
+++ 2-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump :: Hey Mask, How are you?"
+echo "Mars  :: Hello Trump, I am fine. How are you doing"
+echo "Trump :: I am fine too. What's up?"
+echo "Mars  :: Nothing. Just going to Mars now. Will you come?"
+echo "Trump :: Sorry. Carry on!. I will come once you come back."
