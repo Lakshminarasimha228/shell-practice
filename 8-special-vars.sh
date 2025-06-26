f42960c84ff465bc4820633efebf41af1351b52a
lakshmi <lakshminarasimha228@gmail.com>
2025-06-26 22:40:31 +0530

    special vars

--- 8-special-vars.sh
+++ 8-special-vars.sh
@@ -1,5 +1,11 @@
 #!/binn/bash
 
 echo "All variables passed to the script: $@"
-
 echo "number of variable: $#"
+echo " Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of user: $HOMe"
+echo "PID of the script: $S"
+sleep 10 &
+echo "PID of the next command in background: $!"
