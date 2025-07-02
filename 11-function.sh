3c83dd46ed18b522d88f98de35cd425b26bbb543
lakshmi <lakshminarasimha228@gmail.com>
2025-07-02 23:15:39 +0530

    special vars

--- 11-function.sh
+++ 11-function.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USERID+$(id -u)
+USERID=$(id -u)
 
-if [$USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then 
      echo "ERROR:: Please run this script with root access"
     exit 1 # give other than 0 upon 127
